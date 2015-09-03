ef9aaf79bcd399f90110c2d9ac7e74a8fe6c8109
Dmitry Osipenko <digetx@gmail.com>
2015-09-03 18:07:55 +0300

    ARM: tegra: picasso: Add reboot handler
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

--- arch/arm/mach-tegra/board-picasso.c
+++ arch/arm/mach-tegra/board-picasso.c
@@ -18,7 +18,10 @@
 
 #include <linux/gpio/machine.h>
 #include <linux/platform_device.h>
+#include <linux/reboot.h>
 #include <linux/rfkill-gpio.h>
+#include <linux/syscalls.h>
+#include <linux/uaccess.h>
 
 static struct rfkill_gpio_platform_data wifi_rfkill_platform_data = {
 	.name	= "wifi_rfkill",
@@ -63,6 +66,47 @@ static struct gpiod_lookup_table bluetooth_gpio_lookup = {
 	},
 };
 
+static void picasso_reboot_cmd(char *cmd)
+{
+	struct file *fp;
+	mm_segment_t oldfs;
+
+	fp = filp_open("/dev/block/mmcblk0p5", O_WRONLY | O_LARGEFILE, 0);
+	if (WARN_ON(!fp))
+		return;
+
+	oldfs = get_fs();
+	set_fs( get_ds() );
+
+	pr_emerg("%s: %s\n", __func__, cmd);
+	fp->f_op->write(fp, cmd, strlen(cmd), &fp->f_pos);
+
+	set_fs(oldfs);
+	filp_close(fp, 0);
+
+	sys_sync();
+}
+
+static int picasso_reboot_event(struct notifier_block *notifier,
+				unsigned long event, void *cmd)
+{
+	if (event == SYS_RESTART && cmd != NULL) {
+		if (!strncmp(cmd, "recovery", 8))
+			picasso_reboot_cmd("FOTA");
+		else if (!strncmp(cmd, "bootloader", 10))
+			picasso_reboot_cmd("FastbootMode");
+		else if (!strncmp(cmd, "bootmenu", 8))
+			picasso_reboot_cmd("BootmenuMode");
+	}
+
+	return NOTIFY_OK;
+}
+
+static struct notifier_block picasso_reboot_notifier = {
+	.notifier_call = picasso_reboot_event,
+	.priority = INT_MAX,
+};
+
 void __init tegra_picasso_rfkill_init(void)
 {
 	gpiod_add_lookup_table(&wifi_gpio_lookup);
@@ -70,4 +114,6 @@ void __init tegra_picasso_rfkill_init(void)
 
 // 	platform_device_register(&wifi_rfkill_device);
 	platform_device_register(&bluetooth_rfkill_device);
+
+	register_reboot_notifier(&picasso_reboot_notifier);
 }
