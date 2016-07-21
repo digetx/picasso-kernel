83949608b53464be94d83ce4c40a10f8d56fb34c
Dmitry Osipenko <digetx@gmail.com>
2016-07-21 23:26:40 +0300

    Revert "netfilter: x_tables: kill check_entry helper"
    This reverts commit dda467549d44f4d39b4420511ddcf77d29e9b6b2.

--- net/ipv4/netfilter/arp_tables.c
+++ net/ipv4/netfilter/arp_tables.c
@@ -490,6 +490,14 @@ static int mark_source_chains(const struct xt_table_info *newinfo,
 	return 1;
 }
 
+static inline int check_entry(const struct arpt_entry *e)
+{
+	if (!arp_checkentry(&e->arp))
+		return -EINVAL;
+
+	return xt_check_entry_offsets(e, e->target_offset, e->next_offset);
+}
+
 static inline int check_target(struct arpt_entry *e, const char *name)
 {
 	struct xt_entry_target *t = arpt_get_target(e);
@@ -579,10 +587,7 @@ static inline int check_entry_size_and_hooks(struct arpt_entry *e,
 		return -EINVAL;
 	}
 
-	if (!arp_checkentry(&e->arp))
-		return -EINVAL;
-
-	err = xt_check_entry_offsets(e, e->target_offset, e->next_offset);
+	err = check_entry(e);
 	if (err)
 		return err;
 
@@ -1241,10 +1246,8 @@ check_compat_entry_size_and_hooks(struct compat_arpt_entry *e,
 		return -EINVAL;
 	}
 
-	if (!arp_checkentry(&e->arp))
-		return -EINVAL;
-
-	ret = xt_check_entry_offsets(e, e->target_offset, e->next_offset);
+	/* For purposes of check_entry casting the compat entry is fine */
+	ret = check_entry((struct arpt_entry *)e);
 	if (ret)
 		return ret;
 
